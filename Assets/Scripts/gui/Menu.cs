519c35bee408a8485dde898dc01b3559a408a59d
iorlov <nchanged@gmail.com>
2014-01-25 21:01:27 +0200

    Adding menu adn placing cannon

---
+++ Assets/Scripts/gui/Menu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void OnGUI(){
+
+
+		GUI.Box (new Rect (0,Screen.height - 45,Screen.width,50), "");
+		// Cannon button
+		if(GUI.Button(new Rect(10,Screen.height - 35,100,25), "Cannon")) {
+			Debug.Log("Clicked");
+
+			GameObject cannon;
+
+			cannon = GameObject.Find("Cannon");
+			Vector3 pos = new Vector3(450, 0, 400);
+			Instantiate(cannon, pos, transform.rotation);
+
+
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
