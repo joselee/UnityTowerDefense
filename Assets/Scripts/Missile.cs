7b1ad4c10ed94d2ffa51279268be3d4a17e59880
Jose Lee <jose.lee@conmio.com>
2014-01-25 23:21:03 +0200

    Anti air logic

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour {
+
+	public float Speed;
+	public float Range;
+	//public GameObject ExplosionEffect;
+	public Transform Target;
+
+	
+	private float currentDistance;
+	
+	void Start () {
+	}
+	
+	void Update () {
+		// Move the cannonball "Forward" along the Z-axis
+		transform.Translate(Vector3.forward * Time.deltaTime * this.Speed);
+		
+		// Calculate how much distance the cannonball has moved
+		currentDistance += Time.deltaTime * this.Speed;
+		
+		// Destroy the cannonball if it has reached its max range
+		if(currentDistance >= this.Range)
+		{
+			Debug.Log("Traveled past max range");
+			explode();
+		}
+
+		if(this.Target)
+		{
+			transform.LookAt(this.Target);
+		}
+		else 
+		{
+			Debug.Log("Lost target.");
+			explode();
+		}
+	}
+
+	public void OnTriggerEnter(Collider enteringObject)
+	{
+		if(enteringObject.gameObject.tag == "Air_Enemy")
+		{
+			//In the future, we do damage to the enemy like this:
+			// enteringObject.gameObject.dealDamage(this.DamageAmount);
+
+			Debug.Log("Hit enemy!");
+			explode ();
+		}
+	}
+
+	private void explode()
+	{
+		//Instantiate(this.ExplosionEffect, transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/missileLauncher.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class missileLauncher : MonoBehaviour {
+
+	public GameObject Ammunition;
+	public float FireRate = 1f;
+	public Transform MissileSpawnPoint;
+	public Transform MissileLauncherBody;
+	public Transform MissileLauncherHead;
+	
+	private Transform enemyTarget;
+	private float nextFireTime;
+	
+	void Start () {}
+	
+	void Update () {
+		// Enemy is in range.
+		if(enemyTarget)
+		{
+			MissileLauncherHead.LookAt(enemyTarget.position);
+			
+			if(Time.time > nextFireTime)
+			{
+				FireMissile();
+			}
+		}
+	}
+	
+	void OnTriggerEnter(Collider enteringObject){
+		// Automatically by Unity when any object enters the current game gameObject's collider.
+		if(enteringObject.gameObject.tag == "Air_Enemy")
+		{
+			nextFireTime = Time.time;
+			enemyTarget = enteringObject.gameObject.transform;
+		}
+	}
+	
+	void OnTriggerExit(Collider exitingObject){
+		// Automatically by Unity when any object leaves the current gameObject's collider.
+		if(exitingObject.gameObject.transform == enemyTarget)
+		{
+			enemyTarget = null;
+		}
+	}
+	
+	void FireMissile(){
+		nextFireTime = Time.time + FireRate;
+
+		//Create a missile in the correct position and rotation
+		GameObject missileObject = Instantiate(Ammunition, MissileSpawnPoint.position, MissileLauncherHead.rotation) as GameObject;
+		//missileObject.GetComponent(Missile).Target = this.enemyTarget;
+
+
+		Missile missile = (Missile) missileObject.GetComponent<Missile>();
+		missile.Target = this.enemyTarget;
+	}
+}
