a6d6eb0b8541a7d36f64239269db4fcf4c1c0779
iorlov <nchanged@gmail.com>
2014-01-25 23:06:32 +0200

    Adding missilelauncher to menu

--- Assets/Scripts/gui/Menu.cs
+++ Assets/Scripts/gui/Menu.cs
@@ -16,14 +16,25 @@ public class Menu : MonoBehaviour {
 		return GUI.Button(new Rect(10,Screen.height - 35,100,25), text);
 	}
 
+	bool missileButton()
+	{
+		return GUI.Button(new Rect(120,Screen.height - 35,120,25), "MissileLauncher");
+	}
+
+
 	void OnGUI(){
 		GUI.Box (new Rect (0,Screen.height - 45,Screen.width,50), "");
 		// Cannon button
 		if(cannonButton(cannonButtonCaption)) {
 			// Storing the object
 			selectedObject = GameObject.Find("Cannon");
-			cannonButtonCaption = "Place object";
 			Debug.Log("Cannong selected");
+		}
+
+		if(missileButton()) {
+			// Storing the object
+			selectedObject = GameObject.Find("MissileLauncher");
+			Debug.Log("MissileLauncher selected");
 		}
 	}
 
@@ -32,14 +43,13 @@ public class Menu : MonoBehaviour {
 
 		if(Input.GetMouseButtonDown(0) && selectedObject){
 			RaycastHit hit;
+
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			Plane hPlane = new Plane(Vector3.up, Vector3.zero);
 			float distance = 0; 
-
 			if (hPlane.Raycast(ray, out distance)){
 				Instantiate(selectedObject,ray.GetPoint(distance), transform.rotation);
 				selectedObject = null;
-				cannonButtonCaption = "Cannon";
 			}
 		}
 	}
