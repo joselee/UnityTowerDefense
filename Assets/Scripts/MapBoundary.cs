b6a72d40338733ac4ea41f496638b2678cc21227
Jose Lee <jose.lee@conmio.com>
2014-01-27 17:59:50 +0200

    Add map boundaries which destroys any gameobject it touches

---
+++ Assets/Scripts/MapBoundary.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapBoundary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collisionObject){
+		Destroy(collisionObject.gameObject);
+	}
+
+	void OnTriggerStay(Collider collisionObject){
+		Destroy(collisionObject.gameObject);
+	}
+}
