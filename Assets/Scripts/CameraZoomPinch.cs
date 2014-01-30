997ed38bd990b3944663e43190fb5c840c93134a
iorlov <nchanged@gmail.com>
2014-01-30 17:06:36 +0200

    1

--- Assets/Scripts/CameraZoomPinch.cs
+++ Assets/Scripts/CameraZoomPinch.cs
@@ -29,7 +29,16 @@ public class CameraZoomPinch : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		RaycastHit hit;
 		
+		Ray ray = selectedCamera.ScreenPointToRay(Input.mousePosition);
+		
+		if (Physics.Raycast (ray, out hit)) {
+
+			Debug.DrawRay (ray.origin, ray.direction * 1000, Color.yellow);
+			Debug.Log(hit.collider.name);
+			
+		}
 		// Pinch to zoom
 		if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved) 
 		{
@@ -91,9 +100,36 @@ public class CameraZoomPinch : MonoBehaviour
 					
 					Vector3 newPosition = new Vector3(position.x, 300, position.z);
 					selectedCamera.transform.position = newPosition;
+
+					// Detect wich object is clicked
+					detectClickedObject(Input.mousePosition);
+
 				}
 			}
 			//Input.GetTouch(0).position.x
 		}
+	}
+
+	void detectClickedObject(Vector3 pos)
+	{
+			
+			RaycastHit hit;
+			
+			Ray ray = selectedCamera.ScreenPointToRay(pos);
+			
+			if (Physics.Raycast (ray, out hit)) {
+				Debug.Log(hit.transform.gameObject.name);
+				//Debug.DrawRay (ray.origin, ray.direction * 99999, Color.yellow);
+
+			}
+			/*
+			Plane hPlane = new Plane(Vector3.up, Vector3.zero);
+
+
+			float distance = 0; 
+			if (hPlane.Raycast(ray, out distance)){
+				Vector3 hitPoint = ray.GetPoint(distance);
+
+			}*/
 	}
 }
---
+++ Assets/Scripts/buildings/warFactory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class warFactory : building {
+
+	// Use this for initialization
+	void Start () {
+
+		pukka();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
