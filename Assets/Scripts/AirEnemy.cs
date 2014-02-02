cc0036e78022ed553bac6ff8d250aa4d9cce18a9
Jose Lee <joselee@conmio.com>
2014-02-02 23:29:23 +0200

    add cannon and missile launcher back as prefabs and putthem in menu

--- Assets/Scripts/AirEnemy.cs
+++ Assets/Scripts/AirEnemy.cs
@@ -2,21 +2,28 @@
 using System.Collections;
 
 public class AirEnemy : MonoBehaviour {
-
+	
 	public float Health;
 	public float MinAltitude = 4.0f;
 	public float MaxAltitude = 5.5f;
 	public float MinSpeed = 20.0f;
 	public float MaxSpeed = 25.0f;
-
+	public float healthBarLength = 10.0f;
+	
+	private float maxHealth;
 	private float movementSpeed;
-
+	
 	// Use this for initialization
 	void Start () {
-		Health = 100;
+		Health = 250;
+		
+		maxHealth = Health;
+		
+		healthBarLength = Screen.width / 10;
+		
 		movementSpeed = Random.Range(MinSpeed, MaxSpeed);
-
-
+		
+		
 		Vector3 temp = transform.position;
 		temp.y = Random.Range(MinAltitude, MaxAltitude);
 		transform.position = temp;
@@ -26,19 +33,33 @@ public class AirEnemy : MonoBehaviour {
 	void Update () {
 		transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
 	}
-
+	
 	public void TakeDamage (float damageAmount)
 	{
 		Health -= damageAmount;
+		
+		AdjustHealthBar();
+		
 		if(Health <= 0)
 		{
 			explode ();
 			return;
 		}
 	}
-
+	
 	private void explode()
 	{
 		Destroy(gameObject);
+	}
+	
+	void OnGUI() {
+		Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+		GUI.Box(new Rect((screenPos.x - healthBarLength/2), (Screen.height - screenPos.y) - 50, healthBarLength, 20), Health + "/" + maxHealth);
+		
+	}
+	
+	public void AdjustHealthBar() {
+		
+		healthBarLength = (Screen.width / 10) * (Health /(float)maxHealth);
 	}
 }
