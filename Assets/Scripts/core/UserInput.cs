2d617d2e9ecaec7c87926819aae3a3a5c48b2209
Jose Lee <jose.lee@conmio.com>
2014-02-04 11:02:47 +0200

    Camera smooth deceleration done with Physics AddForce instead of Lerp

--- Assets/Scripts/core/UserInput.cs
+++ Assets/Scripts/core/UserInput.cs
@@ -5,27 +5,20 @@ using System;
 
 public class UserInput : MonoBehaviour {
 
-
 	private bool lockCameraMovement = false;
 	private Vector3 hitPosition = Vector3.zero;
 	private Vector3 cameraStartPosition = Vector3.zero;
 	private Vector3 cameraMovePosition;
-	private Vector3 latestDirection;
 	private bool stopCameraAnimation = false;
-	public float CameraSpeed = 1f;
 	private float DefaultCameraY = 300;
 
-
+	Vector3 previousFramePosition;
+	Vector3 velocity = Vector3.zero;
 
 
 	// Use this for initialization
 	void Start () {
-
-
-
 		Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, DefaultCameraY,Camera.main.transform.position.z);
-
-
 	}
 	
 	// Update is called once per frame
@@ -35,25 +28,14 @@ public class UserInput : MonoBehaviour {
 		bool userFingerPressed = Input.GetMouseButton(0);
 		Vector3 pointerPosition = Input.mousePosition;
 
-		/*
-		Vector3 a = new Vector3(Camera.main.transform.position.x, DefaultCameraY,Camera.main.transform.position.z);
-		Ray ray = Camera.main.ScreenPointToRay(a);
-		RaycastHit hit;
-		if (Physics.Raycast (ray,  out hit)) {
-			Debug.DrawLine (ray.origin, hit.point, Color.yellow);
-		}*/
+		// First check if we're selecting/dragging an object
+		selectAndDrag(pointerPosition, userFingerUp, userFingerDown);
 
 		// Move the camera
 		if ( lockCameraMovement == false ){
 			moveCamera(userFingerUp, userFingerDown, userFingerPressed, pointerPosition);
 		}
-
-		selectAndDrag(pointerPosition, userFingerUp, userFingerDown);
 	}
-
-	private List<FingerDestination> userFingerMoveHistory
-		= new List<FingerDestination>();	
-
 
 	private bool reachedBoundaries()
 	{
@@ -78,7 +60,7 @@ public class UserInput : MonoBehaviour {
 
 			stopCameraAnimation = true;
 			cameraStartPosition = Camera.main.transform.position;
-
+			rigidbody.velocity = velocity;
 		}
 
 		if ( userFingerPressed ) {
@@ -87,121 +69,29 @@ public class UserInput : MonoBehaviour {
 			pointerPosition.z = hitPosition.z = cameraStartPosition.y;
 
 			// Calculating camera shift
-			Vector3 direction = Camera.main.ScreenToWorldPoint(pointerPosition) 
-				- Camera.main.ScreenToWorldPoint(hitPosition);	
-			direction = direction * -1;
-
-
-
-			userFingerMoveHistory.Add(new FingerDestination(direction));
-
-			latestDirection = direction;
+			Vector3 direction = Camera.main.ScreenToWorldPoint(pointerPosition) - Camera.main.ScreenToWorldPoint(hitPosition);	
+			direction *= -1;
 
 			Vector3 calculatedPosition = cameraStartPosition + direction;
 			cameraMovePosition = new Vector3(calculatedPosition.x, DefaultCameraY, calculatedPosition.z);
 
-
 			Camera.main.transform.position = cameraMovePosition;
 
+			// Calculate the velocity of the camera: Distance traveled since the previous frame / time since previous frame.
+			Vector3 distance = cameraMovePosition - previousFramePosition;
+			velocity = distance/Time.deltaTime;
+			previousFramePosition = cameraMovePosition;
 		}
 
 
-		// User finished draggin
-		// Here we have to calculate the animation
-		if ( userFingerUp) {
-			stopCameraAnimation = false;
-
-			// Calculate speed
-			double speedX = 0;
-			double speedZ = 0;
-			if ( userFingerMoveHistory.Count >= 2) {
-				FingerDestination lastCoord = userFingerMoveHistory[userFingerMoveHistory.Count - 1];
-				FingerDestination prevCoord = userFingerMoveHistory[userFingerMoveHistory.Count - 2];
-
-				float dx = lastCoord.getX() - prevCoord.getX();
-				float dz = lastCoord.getZ() - prevCoord.getZ();
-
-				double dt = lastCoord.getTicks() - prevCoord.getTicks();
-				speedX = Math.Round(dx/dt,3) / 3;
-				speedZ = Math.Round(dz/dt,3) / 3;
-				// Reset coord list
-				userFingerMoveHistory = new List<FingerDestination>();
-			}
-			
-			Vector3 curPos = Camera.main.transform.position;
-			Vector3 shiftDifference = hitPosition - pointerPosition;
-
-			float shiftX = curPos.x;
-			float shiftZ = curPos.z;
-
-
-			shiftZ += (float)speedZ;
-			shiftX += (float)speedX; 
-
-
-			animationDistance = new Vector3(shiftX, DefaultCameraY,shiftZ);
-			cameraStartPosition = Camera.main.transform.position - animationDistance;
-			StartCoroutine(SmoothCameraMoveOnTouchEnd());
+		// User finished draggin.. slide the camera to a stop using Rigidbody physics
+		if ( userFingerUp ) {
+			rigidbody.AddForce(velocity, ForceMode.VelocityChange);
+			velocity = Vector3.zero;
 		}
 	}
 
-	public static float Round(float value, int digits)
-	{
-		float mult = Mathf.Pow(10.0f, (float)digits);
-		return Mathf.Round(value * mult) / mult;
-	}
-
-	private class FingerDestination
-	{
-		Vector3 pos;
-		double time;
-		public FingerDestination(Vector3 pos)
-		{
-			this.pos = pos;
-			this.time = (DateTime.Now - new DateTime(1970, 1, 1).ToLocalTime()).TotalSeconds;
-		}
-		public float getX()
-		{
-			return pos.x;
-		}
-		public float getZ()
-		{
-			return pos.z;
-		}
-		public double getTicks()
-		{
-			return time;
-		}
-	}
-
-
-
-	private float transitionDuration = 2.5f;
-	private Vector3 animationDistance;
-	IEnumerator SmoothCameraMoveOnTouchEnd()
-	{
-		float t = 0.1f;
-
-		Vector3 distance = Camera.main.transform.position - animationDistance;
-		Vector3 startingPos = Camera.main.transform.position;
-		/*
-		float speedX = distance.x / cameraStartPosition.x;
-		float speedY = distance.x / cameraStartPosition.x;
-		float commonSpeed = speedX + speedY;*/
-	
-		while (t < 1f && stopCameraAnimation == false)
-		{
-
-
-			t += Time.deltaTime * CameraSpeed;//(Time.timeScale/transitionDuration);
-
-			transform.position = Vector3.Lerp(startingPos,animationDistance, t);
-			yield return 0;
-		}
-	}
-
-
-
+	/*----- Moving a draggable object below ------*/
 	private IDraggable draggableComponent = null;
 	private Vector3 latestDragCameraPosition;
 	private Vector3 latestSelectCameraPosition;
