2dc633ed84af5e2fdd52b78d9f057dddf0b73b67
iorlov <nchanged@gmail.com>
2014-02-05 15:56:22 +0200

    Adding GUi Camera

---
+++ Assets/Scripts/gui/GuiCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiCamera : MonoBehaviour {
+
+	public Camera camera;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.camera.orthographicSize  = Screen.height / 2;
+	}
+}
