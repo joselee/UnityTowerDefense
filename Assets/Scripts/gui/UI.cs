ff692b8867cfddd6286f7d8a24daa92e8dbf8648
iorlov <nchanged@gmail.com>
2014-02-12 22:00:31 +0200

    WTF

--- Assets/Scripts/gui/UI.cs
+++ Assets/Scripts/gui/UI.cs
@@ -3,27 +3,25 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class UI : MonoBehaviour {
-
+	
 	//private static List<GameObject> createdGameObjects
 	//	= new List<GameObject>();
-
+	
 	private static  Dictionary<string,UIObject> instances 
 		= new Dictionary<string, UIObject>();
-
+	
 	public Camera camera;
 	public static GameObject group;
 	public GameObject GuiGroup;
-
+	
 	public static GameObject attach(UIObject o)
 	{
-		float realPositionX = (Screen.width / 2 - o.getWidth()/2)*-1;
-		float realPositionY = (Screen.height / 2 - o.getHeight()/2)*-1;
+		
 		Vector2 userPosition = o.getPosition();
-
-
+		
+		
 		Vector3 correctedPosition 
-			= new Vector3( userPosition.x,  userPosition.y, 1);
-		Debug.Log(correctedPosition);
+			= new Vector3( userPosition.x, userPosition.y, 1);
 		
 		GameObject inst 
 			= Instantiate(Resources.Load(o.getResourceName()), correctedPosition, Quaternion.identity) as GameObject;
@@ -34,29 +32,7 @@ public class UI : MonoBehaviour {
 		inst.name = o.getName();
 		return inst;
 	}
-
-	public static float convertY(float y, float h)
-	{
-		return (Screen.height / 2 - h/2)*-1 + y;
-	}
-
-	public static float convertX(float x, float w)
-	{
-		return (Screen.height / 2 -w/2)*-1 + x;
-	}
-
-
-
-	public static bool animateFromBottom(GameObject o, float targetHeigh)
-	{
-		Vector3 pos = o.transform.position;
-		if ( pos.y < targetHeigh ){
-
-			o.transform.position = new Vector3(pos.x, pos.y++, pos.z);
-			return false;
-		}
-		return true;
-	}
+	
 	// Use this for initialization
 	void Start () {
 		if (  group == null ){
@@ -67,8 +43,7 @@ public class UI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.camera.orthographicSize  = Screen.height / 2;
-		this.camera.transform.position = new Vector3(Screen.width / 2, Screen.height /2 );
-		//GuiGroup.transform.localScale = new Vector3(Screen.width, Screen.height, 1);
-		GuiGroup.transform.position = new Vector3(Screen.width/2, Screen.height/2, 1);
+		this.camera.transform.position = new Vector3(Screen.width/2, Screen.height/2, 0);
 	}
 }
+
--- Assets/Scripts/gui/UnitBar.cs
+++ Assets/Scripts/gui/UnitBar.cs
@@ -2,15 +2,15 @@
 using System.Collections;
 
 public class UnitBar : MonoBehaviour {
-
+	
 	private static GameObject researchButton;
 	private static GameObject buildButton;
 	public static bool hideButtons = false;
 	public static bool inited = false;
-
+	
 	public static void show()
 	{
-
+		
 		if ( researchButton != null ){
 			GameObject.Destroy(researchButton);
 			Debug.Log("removing buttons");
@@ -18,28 +18,28 @@ public class UnitBar : MonoBehaviour {
 		if ( buildButton != null ){
 			GameObject.Destroy(buildButton);
 		}
-
+		
 		int bWidth = 75;
 		UIObject _researchButton = new UIObject("gui/research-button", "research-button", bWidth, bWidth );
 		_researchButton.setPosition(new Vector2(Screen.width/2 - bWidth - 15,-95));
 		researchButton = UI.attach(_researchButton);
-
+		
 		
 		UIObject bButton = new UIObject("gui/build-button", "build-button", bWidth, bWidth );
 		bButton.setPosition(new Vector2(Screen.width / 2,-95));
 		buildButton = UI.attach(bButton);
-
+		
 		hideButtons = false;
 	}
-
+	
 	public static void hide()
 	{
 		hideButtons = true;
-
+		
 	}
 	public float transitionDuration = 2.5f;
-
-
+	
+	
 	IEnumerator Transition(GameObject o, Vector3 target)
 	{
 		float t = 0.0f;
@@ -51,32 +51,33 @@ public class UnitBar : MonoBehaviour {
 			yield return 0;
 		}
 	}
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
-
+	
 	// Update is called once per frame
 	void Update () {
-
-
+		
+		
 		if (UnitBar.researchButton != null && UnitBar.buildButton != null) {
-
+			
 			float dest = 0f;
 			if ( hideButtons ) {
 				dest = -120;
-
+				
 			} else {
 				dest = 50;
 			}
 			Vector3 rPos = new Vector3(researchButton.transform.position.x, dest, 1 );
 			StartCoroutine(Transition(researchButton, rPos));
-
+			
 			Vector3 bPos = new Vector3(buildButton.transform.position.x, dest, 1 );
 			StartCoroutine(Transition(buildButton, bPos));
 		}
 	}
 }
+
